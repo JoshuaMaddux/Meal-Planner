fc567bbceac4d0ceda593fc09af72659ab268af9
JoshuaMaddux <joshua@95visual.com>
2026-04-12 20:40:35 -0700

    Create data.js
    Fixed dir J's name issue... Dang auto correct.

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const THEMES = [
+  {id:'classic',name:'Classic',emoji:'🏠'},
+  {id:'mexican',name:'Mexican',emoji:'🇲🇽'}
+];
+
+const BASE_WEEKS = [
+{
+  season:'Winter',
+  protein:'Turkey',
+  themeId:'classic',
+  days:[
+    {n:'Sunday',m:'Turkey Bowls'},
+    {n:'Monday',m:'Turkey Rice'},
+    {n:'Tuesday',m:'Tacos',anchor:'taco'},
+    {n:'Friday',m:'Pizza',anchor:'pizza'}
+  ]
+}
+];
+
+const TOTAL_WEEKS = 26;
